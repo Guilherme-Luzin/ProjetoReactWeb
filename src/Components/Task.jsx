e16c0f077006771d05e77702d5d6b44eec51917a
Guilherme Luzin <guilherme.carvalho@inventsoftware.com.br>
2025-06-08 19:49:12 -0300

    Adicionando icone para quando completar a tarefa

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react"
+import { CheckIcon, ChevronRightIcon, TrashIcon } from "lucide-react"
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
@@ -20,10 +20,11 @@ function Task({tasks, onTaskClick, onDeleteTaskClick}) {
             <button
               onClick={() => onTaskClick(task.id)}
               className={
-                `bg-slate-400 text-white p-2 rounded-md w-full text-left 
+                `bg-slate-400 text-white p-2 rounded-md w-full flex items-center gap-2 text-left 
                 ${task.isCompleted && "line-through"}`
               }
             >
+              {task.isCompleted && <CheckIcon />}
               {task.title}
             </button>
             <Button
